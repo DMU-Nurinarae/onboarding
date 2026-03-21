2d3e29ec459fa3e132e6ef54e393ef275146181f
stop16 <whitelyjjh8230@kakao.com>
2026-03-22 04:34:10 +0900

    feat: patch for release

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dmu-nurinarae.github.io',
+  base: '/onboarding',
   integrations: [
     starlight({
       title: "누리나래 온보딩",
