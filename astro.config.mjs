7ae16e49f90fe395137db1def34fed39e70155a6
stop16 <whitelyjjh8230@kakao.com>
2026-03-22 04:28:43 +0900

    add: orientation content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,42 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "누리나래 온보딩",
+      customCss: ["./src/styles/glass.css"],
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/DMU-Nurinarae/",
+        },
+      ],
+      sidebar: [
+        {
+          label: "오리엔테이션",
+          items: [
+            { label: "들어가며", slug: "guides/intro" },
+            { label: "살펴보기", slug: "guides/overview" },
+            { label: "개발 문화", slug: "guides/dev_culture" },
+          ],
+        },
+        {
+          label: "캐드와 3D 프린팅",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "캐드 알아보기", slug: "cad/intro" },
+            { label: "Fusion 설치하기", slug: "cad/install" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
 });
---
+++ src/content/docs/cad/install.md
@@ -0,0 +1,5 @@
+---
+title: Fusion 설치하기
+description: Guides how to install Fusion
+---
+#### Fusion은 뭔가요?
